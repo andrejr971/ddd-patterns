0ddfff4754495fd6939d134da448a571c96275db
André Junior <andrejunior179@gmail.com>
2022-09-14 16:37:14 -0300

    add test and aggregates

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Address } from "./entity/address";
+import { Customer } from "./entity/customer";
+import { Order } from "./entity/order";
+import { OrderItem } from "./entity/order_item";
+
+let customer = new Customer('123', 'André');
+const address = new Address('Rua dois', 2, '1234-000', 'São Paulo');
+customer.Address = address;
+customer.activate();
+
+const item1 = new OrderItem('1', 'Item 1', 10, 'productId', 1);
+const item2 = new OrderItem('2', 'Item 2', 20, 'productId', 3);
+
+const order = new Order('1', '123', [item1, item2]);
